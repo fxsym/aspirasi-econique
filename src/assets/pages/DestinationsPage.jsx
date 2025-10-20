8af4c1ef8b3c64584349276f40fe49bf10bbdd82
fxsym <fsyamsudin24@gmail.com>
2025-10-20 20:04:23 +0700

    Fix page header

--- src/assets/pages/DestinationsPage.jsx
+++ src/assets/pages/DestinationsPage.jsx
@@ -1,11 +1,11 @@
-import DestinationPageHeader from "../components/DestinationsPageHeader";
 import Navbar from "../components/Navbar";
+import PageHeader from "../components/PageHeader";
 
 export default function DestinationsPage() {
     return (
         <div>
             <Navbar />
-            <DestinationPageHeader />
+            <PageHeader title={'Destinasi Kami'} slogan={'Nikmati Setiap Momen, \nCiptakan Kenangan.'} />
         </div>
     )
 }
