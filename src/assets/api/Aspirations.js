6912ec6f39638f55bb8c97788b520a8453e36d33
fxsym <fsyamsudin24@gmail.com>
2025-11-05 14:07:07 +0700

    Add function to get aspirations data from api

--- src/assets/api/Aspirations.js
+++ src/assets/api/Aspirations.js
@@ -2,6 +2,19 @@ import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:8000/api/'
 
+export const getAspirations = async (data) => {
+    try {
+        const response = await axios.get(`${API_URL}aspirations`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        return response
+    } catch (error) {
+        throw error.response
+    }
+}
+
 export const createAspiration = async (data) => {
     try {
         const response = await axios.post(`${API_URL}aspirations`, data, {
--- src/assets/pages/Dashboard.jsx
+++ src/assets/pages/Dashboard.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
+import { getAspirations } from "../api/Aspirations"
+
 export default function Dashboard() {
+    const [aspirations, setAspirations] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const fetchAspirations = async () => {
+        setLoading(true)
+        setError(null)
+        try {
+            const response = await getAspirations()
+            setAspirations(response.data)
+            console.log(response)
+        } catch (error) {
+            setError(error)
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchAspirations()
+    }, [])
+
     return (
         <div>
             <p>Ini dashboard admin</p>
+            <div>
+                Total Aspirasi saat ini : {aspirations.length}
+            </div>
         </div>
     )
 }
